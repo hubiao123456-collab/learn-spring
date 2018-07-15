43e9a8ca4bbab1deaf58d63d0537386ee6b448f1
letian <sunlt1699@gmail.com>
2018-07-15 10:08:17 +0800

    up

--- spring-demo-001/src/main/java/demo/Main.java
+++ spring-demo-001/src/main/java/demo/Main.java
@@ -6,8 +6,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Main {
 
     public static void main(String[] args) {
-        ApplicationContext ctx =
-                new ClassPathXmlApplicationContext("beans.xml");
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("beans.xml");
         SayHello sayHello = (SayHello) ctx.getBean("sayHello");
         System.out.println(sayHello.sayHello("world"));
     }
