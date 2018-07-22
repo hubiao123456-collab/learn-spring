49dc13271e8b84808704142b605f3e8d6a6d5db2
letian <sunlt1699@gmail.com>
2018-07-22 09:32:25 +0800

    up

--- spring-boot-demo-0008/src/main/java/hello/CustomExceptionHandler.java
+++ spring-boot-demo-0008/src/main/java/hello/CustomExceptionHandler.java
@@ -1,4 +1,40 @@
 package hello;
 
-public class CustomExceptionHandler {
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Set;
+
+@ControllerAdvice
+public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
+
+//    @Override
+//    protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(
+//            HttpRequestMethodNotSupportedException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+//
+//        Set<HttpMethod> supportedMethods = ex.getSupportedHttpMethods();
+//        if (!CollectionUtils.isEmpty(supportedMethods)) {
+//            headers.setAllow(supportedMethods);
+//        }
+//        return handleExceptionInternal(ex, null, headers, status, request);
+//    }
+
+    @Override
+    protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(
+            HttpRequestMethodNotSupportedException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+        Set<HttpMethod> supportedMethods = ex.getSupportedHttpMethods();
+        if (!CollectionUtils.isEmpty(supportedMethods)) {
+            headers.setAllow(supportedMethods);
+        }
+        return handleExceptionInternal(ex, BaseResponse.fail("请求方法不支持", null), headers, status, request);
+    }
+
 }
