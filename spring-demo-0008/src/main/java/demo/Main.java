8c7de8fc414a0613abf7447efae282e4c9394b9d
letian <sunlt1699@gmail.com>
2019-03-06 09:24:08 +0800

    update spring-boot-demo-0001

--- spring-demo-0008/src/main/java/demo/Main.java
+++ spring-demo-0008/src/main/java/demo/Main.java
@@ -6,10 +6,23 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void useXml() {
         ApplicationContext ctx = new ClassPathXmlApplicationContext("beans.xml");
         Calculate calculate = (Calculate) ctx.getBean(Calculate.class);
         System.out.println(calculate.add(1,2));
+    }
+
+    public static void useAnnotation() {
+        ApplicationContext ctx = new AnnotationConfigApplicationContext(AppConfig.class);
+        Calculate calculate = (Calculate) ctx.getBean(Calculate.class);
+        System.out.println(calculate.add(1,2));
+    }
+
+    public static void main(String[] args) {
+
+        useXml();
+        useAnnotation();
+
     }
 
 }
--- spring-demo-0008/src/test/java/demo/BaseTest.java
+++ spring-demo-0008/src/test/java/demo/BaseTest.java
@@ -1,5 +1,7 @@
 package demo;
 
+import org.junit.Assert;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -7,5 +9,10 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes=AppConfig.class)
 public class BaseTest {
+
+    @Test
+    public void test() {
+
+    }
 
 }
