336ff869a62e7d0e757f9dcc0124714792a29560
letiantian <sunlt1699@gmail.com>
2020-02-09 15:03:43 +0800

    添加示例

---
+++ spring-framework-5.2.2.RELEASE/demo-001/src/main/java/demo/Demo.java
@@ -0,0 +1,14 @@
+package demo;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("beans.xml");
+		SayHello sayHello = (SayHello) ctx.getBean("sayHello");
+		System.out.println(sayHello.sayHello("world"));
+	}
+
+}
